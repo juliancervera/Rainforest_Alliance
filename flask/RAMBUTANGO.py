5394848806e1d7f8210a31f19953b3042893a20b
juliancervera <giordanocervera@gmail.com>
2023-07-03 21:50:45 -0600

    Add new pages to app, reach difficult formats

--- flask/RAMBUTANGO.py
+++ flask/RAMBUTANGO.py
@@ -1,4 +1,8 @@
 #ToDo: Mantener separadas las variables de costos por categoría hasta sumarlas al último
+#ToDo: Add a character limit to every input cell.
+
+# La app va a funcionar con un número finito de actividades que formen parte del proceso productivo.
+
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
@@ -23,19 +27,44 @@ def ventas():
 
         session['producto'] = producto
         session['cantidad_vendida'] = cantidad_vendida
+        session['unidad'] = unidad
         session['precio_venta'] = precio_venta
         session['ingreso_ventas'] = ingreso_ventas
-
+ 
         return render_template("ventas.html", producto=producto, cantidad_vendida=cantidad_vendida,
                                unidad=unidad, precio_venta=precio_venta, ingreso_ventas=ingreso_ventas)
     else:
         return render_template("ventas.html")
 
 
-@app.route("/next_page")
-def next_page():
-    # Logic for the next page goes here
-    return "Next Page"
+@app.route("/pregunta_donativos")
+def pregunta_donativos():
+    producto = session.get('producto')
+    return render_template('pregunta_donativos.html', producto=producto)
+
+
+@app.route("/info_donativos")
+def info_donativos():
+    # Process the request or perform any other necessary actions
+    return "You chose 'Yes' for receiving donativos."
+
+
+@app.route("/pregunta_primera_actividad", methods=["GET", "POST"])
+def pregunta_primera_actividad():
+    if request.method == "POST":
+        actividad = request.form.get("actividad")
+        session["actividad_1"] = actividad
+        return redirect("/info_actividades")
+    else:
+        producto = session.get("producto")
+        return render_template("pregunta_primera_actividad.html", producto=producto)
+
+
+
+@app.route("/info_actividades")
+def info_actividades():
+    actividad_1 = session.get("actividad_1")
+    return f"The first activity is: {actividad_1}"
 
 
 if __name__ == "__main__":
