02b230098c67d3817939d557ff9b0e7a70fd0f36
juliancervera <giordanocervera@gmail.com>
2023-08-12 20:32:50 -0600

    prepare for deployment

--- flask/RAMBUTANGO.py
+++ flask/RAMBUTANGO.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sys
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = '4prKZ_3$%_ztprd_24_P_!?'
 
 
 @app.route("/")
